dd55c8d5499f3d4c530c8b7913cd86eabd4a383e
idiocy <idiocy@DESKTOP-PP32I9J>
2024-11-14 12:58:36 +0300

    Add project files.

---
+++ ByteCounter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+namespace ByteCounter;
+class Program
+{
+    static void Main()
+    {
+        string path = @"E://Downloads";
+        Console.WriteLine($"Total size: {CountBytes(path)} bytes");
+
+        Console.ReadKey();
+    }
+    static double CountBytes(string path, double size = 0)
+    {
+        if (Directory.Exists(path))
+        {
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files) { size += file.Length; }
+
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (string dir in dirs) { size += CountBytes(dir, size); }
+
+            return size;
+        }
+        return -1;
+    }
+}
